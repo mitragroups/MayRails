7dc0766249377063eabe59a1f08f2e115fb7458f
mitragroups <miftahmuhajir@gmail.com>
2023-05-11 06:52:48 +0000

    Seeding Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Book.create(title: 'Flutter', price: 200000, page: 200, description: 'Flutter adalah pemrograman yang cocok dan mudah untuk mobile')
+Book.create(title: 'Java Springboot', price: 450000, page: 450, description: 'Java Springboot adalah framework yang cocok dan strong performance untuk Aplikasi Web')
+Book.create(title: 'React JS', price: 210000, page: 210, description: 'React JS cocok untuk web')
+Book.create(title: 'Laravel', price: 170000, page: 170, description: 'Laravel adalah framework untuk pemrograman PHP')
